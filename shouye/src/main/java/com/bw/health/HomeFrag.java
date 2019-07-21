5cf5477c1abe6ac665a19fd6933ac5cd6a6c59a7
1207133315 <49475399+1207133315@users.noreply.github.com>
2019-07-21 21:33:13 +0800

    李宁康

--- shouye/src/main/java/com/bw/health/HomeFrag.java
+++ shouye/src/main/java/com/bw/health/HomeFrag.java
@@ -1,40 +1,56 @@
 package com.bw.health;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
+import com.bw.health.adapter.BannerAdapter;
 import com.bw.health.adapter.DepartmentAdapter;
 import com.bw.health.adapter.MationListAdapter;
 
 import com.bw.health.adapter.PlateListAdapter;
 import com.bw.health.bean.BannerBean;
 import com.bw.health.bean.DepartmentBean;
+import com.bw.health.bean.LoginBean;
 import com.bw.health.bean.MationBean;
 import com.bw.health.bean.PlateBean;
 import com.bw.health.bean.Result;
 import com.bw.health.core.DataCall;
 import com.bw.health.core.WDFragment;
+import com.bw.health.dao.LoginBeanDao;
 import com.bw.health.exception.ApiException;
 import com.bw.health.presenter.DepartmentPresenter;
 import com.bw.health.presenter.MationListPresenter;
 import com.bw.health.presenter.PlateListPresenter;
 import com.bw.health.presenter.ShowBannerPresenter;
+import com.bw.health.util.GetDaoUtil;
 import com.bw.health.view.MyImageView;
+import com.bw.health.view.NoslideRecyclerview;
 import com.bw.health.view.ViewPagerStop;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import butterknife.BindView;
@@ -65,10 +81,12 @@ public class HomeFrag extends WDFragment {
 
     @BindView(R2.id.bingzheng)
     LinearLayout bingzheng;
-    @BindView(R2.id.bookView)
-    ViewPager vp;
+    @BindView(R2.id.banner)
+    NoslideRecyclerview banner;
     @BindView(R2.id.yaopin)
     LinearLayout yaopin;
+    @BindView(R2.id.head)
+    ImageView head;
     private ShowBannerPresenter showBannerPresenter;
     private PlateListPresenter plateListPresenter;
     private PlateListAdapter plateListAdapter;
@@ -79,6 +97,14 @@ public class HomeFrag extends WDFragment {
     private DepartmentPresenter departmentPresenter;
     private DepartmentAdapter departmentAdapter;
     private List<MyImageView> list=new ArrayList<>();
+    private BannerAdapter bannerAdapter;
+    @BindView(R2.id.yuandian)
+    RadioGroup yuandian;
+    private List<BannerBean> result;
+    private List<RadioButton> yuanList=new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private int position;
+
     @Override
     public String getPageName() {
         return "首页";
@@ -97,6 +123,12 @@ public class HomeFrag extends WDFragment {
         plateRecycler.setLayoutManager(new GridLayoutManager(getContext(),5));
         unplateRecycler.setLayoutManager(new GridLayoutManager(getContext(),5));
         plateListAdapter = new PlateListAdapter(getContext());
+        linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        banner.setLayoutManager(linearLayoutManager);
+        final PagerSnapHelper pagerSnapHelper = new PagerSnapHelper();
+        pagerSnapHelper.attachToRecyclerView(banner);
+        bannerAdapter = new BannerAdapter(getContext());
+
         showBannerPresenter.reqeust();
 
        plateListPresenter.reqeust();
@@ -138,12 +170,70 @@ public class HomeFrag extends WDFragment {
         });
 
 
+       banner.addOnScrollListener(new RecyclerView.OnScrollListener() {
+           @Override
+           public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+               if (newState == RecyclerView.SCROLL_STATE_IDLE){
+                   for (int i = 0; i < yuanList.size(); i++) {
+                        int position = linearLayoutManager.findFirstVisibleItemPosition();
+                        position = position %yuanList.size();
+                       if (i== position){
+                            yuanList.get(i).setChecked(true);
+                       }else {
+                           yuanList.get(i).setChecked(false);
+                       }
+                   }
+
+               }
+           }
+
+           @Override
+           public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+               super.onScrolled(recyclerView, dx, dy);
+           }
+       });
+        handler.sendEmptyMessageDelayed(0,2000);
+
     }
+
+    public Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            position = linearLayoutManager.findFirstVisibleItemPosition();
+            banner.smoothScrollToPosition(position+1);
+            handler.sendEmptyMessageDelayed(0,2000);
+        }
+    };
+
     public  class Banner implements DataCall<Result<List<BannerBean>>> {
+
+
         @Override
         public void success(Result<List<BannerBean>> data, Object... args) {
+            result = data.getResult();
+            bannerAdapter.setList(data.getResult());
+            banner.setAdapter(bannerAdapter);
+            yuanList.clear();
+            for (int i = 0; i <result.size() ; i++) {
+                 RadioButton radioButton = new RadioButton(getContext());
+                 Bitmap a=null;
+                    radioButton.setButtonDrawable(new BitmapDrawable(a));
+                 radioButton.setBackgroundResource(R.drawable.yuandian_select);
+                 yuanList.add(radioButton);
 
 
+
+
+                //这是布局参数,,刚开始小圆点之间没有距离,所以使用java代码指定宽度高度,并且指定小圆点之间的距离
+                RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(30,30);
+                 params.topMargin=30;
+
+                //放到LinearLayout,也就是圆点布局中
+                yuandian.addView(radioButton,params);
+
+            }
+            yuanList.get(0).setChecked(true);
         }
 
         @Override
@@ -151,6 +241,20 @@ public class HomeFrag extends WDFragment {
             Toast.makeText(getContext(), ""+data.getDisplayMessage(), Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+         LoginBeanDao userDao = GetDaoUtil.getGetDaoUtil().getUserDao();
+         List<LoginBean> loginBeans = userDao.loadAll();
+         if (loginBeans.size()>0){
+             Glide.with(getContext()).load(loginBeans.get(0).getHeadPic())
+                     .apply(RequestOptions.bitmapTransform(new CircleCrop()))
+                     .into(head);
+         }
+
+    }
+
     @OnClick({R2.id.bingzheng,R2.id.yaopin,R2.id.head})
     public void yaopinOrbingzheng(View view){
         Bundle bundle = new Bundle();
