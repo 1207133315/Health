04d84473d2ff4b235710fe368e4e89ee4b569fcd
gouyajie-xy <2735490664@qq.com>
2019-07-30 19:59:27 +0800

    guoyajie

--- mine/src/main/java/com/wd/health/activity/PatientsCircleActivity.java
+++ mine/src/main/java/com/wd/health/activity/PatientsCircleActivity.java
@@ -1,13 +1,9 @@
 package com.wd.health.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.ImageView;
-
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.bw.health.core.WDActivity;
 import com.wd.health.R;
+
 /**
  * @Auther: 郭亚杰
  * @Date:2019/7/30
@@ -25,10 +21,12 @@ public class PatientsCircleActivity extends WDActivity {
 
     @Override
     protected void initView() {
+
     }
 
     @Override
     protected void destoryData() {
+
 
     }
 }
