97bafc4a1e01de54d1e09ac5ff98608f565c2352
gouyajie-xy <2735490664@qq.com>
2019-07-30 11:41:37 +0800

    guoyajie

--- mine/src/main/java/com/wd/health/activity/PatientsCircleActivity.java
+++ mine/src/main/java/com/wd/health/activity/PatientsCircleActivity.java
@@ -3,10 +3,16 @@ package com.wd.health.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.bw.health.core.WDActivity;
 import com.wd.health.R;
+/**
+ * @Auther: 郭亚杰
+ * @Date:2019/7/30
+ * @Description: 我的病友圈
+ */
 @Route(path = "/PatientsCircleActivity/")
 public class PatientsCircleActivity extends WDActivity {
 
@@ -17,7 +23,6 @@ public class PatientsCircleActivity extends WDActivity {
 
     @Override
     protected void initView() {
-
     }
 
     @Override
